820789971d5d80a20066ccadb6bd980f559c0483
Mohamed Shaban <57798592+mohamedshabban@users.noreply.github.com>
2024-03-09 15:19:16 +0200

    Add project files.

---
+++ InterfaceSegregationPrincipleInCSharp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace InterfaceSegregationPrincipleInCSharp
+{
+    /// <summary>
+    /// The Interface Segregation Principle (ISP) is one of the SOLID principles,
+    /// and it suggests that a class should not be forced to i
+    /// mplement interfaces it does not use. Instead of having a single, 
+    /// large interface, it's better to have several small,
+    /// specific interfaces. Here's an example in C# to illustrate ISP:
+    /// </summary>
+    /// 
+
+
+    // Interface with both drawing and resizing methods
+    interface IShape
+    {
+        void Draw();
+        void Resize(int width, int height);
+    }
+    public interface IDraw
+    {
+        void Draw();
+    }
+    public interface IResizeable
+    {
+        void Resize(int width, int height);
+    }
+    public class Shape : IDraw, IResizeable
+    {
+        public void Draw()
+        {
+            Console.WriteLine($"Draw {typeof(Shape)}");
+        }
+
+        public void Resize(int width, int height)
+        {
+            Console.WriteLine($"Width: {width} , Height: {height}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
